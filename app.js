fd52db43689de2ed0603a7966a0c6441dc55ab61
Heimar Vega <heimarvega@gmail.com>
2017-10-05 00:00:35 -0500

    Creación de operación get

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //Carga de rutas
-
+var album_routes = require('./routes/album');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -13,5 +13,6 @@ app.use(bodyParser.json());
 //configurar cabeceras
 
 //rutas base
+app.use('/api',album_routes);
 
 module.exports = app;
--- controllers/album.js
+++ controllers/album.js
@@ -1,1 +1,42 @@
 'use strict'
+
+var Album = require('../models/album');
+
+function getAlbum(req,res) {
+    var albumId = req.params.id;
+    Album.findById(albumId,(err,album)=>{
+        if (err) {
+            res.status(500).send({message: 'Error en la petición'});
+        }
+        else{
+            if (!album) {
+                res.status(404).send({message: 'El album no existe'});
+            }
+            else{
+                res.status(200).send(album);        
+            }
+            
+        }
+    })    
+}
+
+function getAlbums(req,res) {
+    Album.find({},(err,albums)=>{
+        if (err) {
+            res.status(500).send({message: 'Error en la petición'});
+        }
+        else{
+            if (!albums) {
+                res.status(404).send({message: 'El album no existe'});
+            }
+            else{
+                res.status(200).send(albums);        
+            }
+            
+        }
+    })    
+}
+module.exports= {
+    getAlbum,
+    getAlbums
+};
